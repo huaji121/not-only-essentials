61d670fd28d301b242137dd53452d7c563d44c07
huaji121 <huaji121@outlook.com>
2026-02-01 21:49:32 +0800

    修复了cleanup的信息提示

--- scripts/cleanup.ts
+++ scripts/cleanup.ts
@@ -10,11 +10,13 @@ system.runInterval(
       world.getDimension(MinecraftDimensionTypes.TheEnd),
     ];
 
-    for (let [idx, dim] of dimensions.entries()) {
+    for (let dim of dimensions) {
+      let count = 0;
       for (let entity of dim.getEntities({ type: MOD_ID.of("musket_round") })) {
         entity.remove();
+        count++;
       }
-      console.log(`在维度${dim.id}清理了${idx + 1}个火枪弹实体`);
+      console.log(`在维度${dim.id}清理了${count}个火枪弹实体`);
     }
   },
   3 * 60 * 20 /**三分钟 */
